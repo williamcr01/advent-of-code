964f968232c2d90266950f3386e8b5782fff4386
William Crona <williamcrona@gmail.com>
2026-02-11 16:42:57 +0100

    day 4-1

--- 2025/day-4/day-4-1.c
+++ 2025/day-4/day-4-1.c
@@ -1,27 +1,70 @@
 #include <stdio.h>
+#include <string.h>
 
-char grid[137][137];
+#define SIZE 139
+char grid[SIZE][SIZE];
+
+int check_access(int max_adjacent);
 
 int main() {
-    int count_col = 0;
-    int count_row = 0;
+    memset(grid, '.', sizeof(grid));
+
+    int count_col = 1;
+    int count_row = 1;
     char temp;
+
     while (scanf("%c", &temp) == 1) {
         if (temp == '\n') {
-            count_col = 0;
+            count_col = 1;
             count_row++;
             continue;
         }
         grid[count_row][count_col] = temp;
         count_col++;
     }
-    for (int i = 0; i < 137; i++) {
-        for (int j = 0; j < 137; j++) {
-            printf("%c", grid[i][j]);
-            if (j == 136) {
-                printf("\n");
+    int accessible = check_access(4);
+    printf("%d\n", accessible);
+    return 0;
+}
+
+int check_access(int max_adjacent) {
+    int neighbor_count;
+    int count = 0;
+    for (int row = 1; row < SIZE - 1; row++) {
+        for (int col = 1; col < SIZE - 1; col++) {
+            if (grid[row][col] == '@') {
+                neighbor_count = 0;
+                if (grid[row - 1][col - 1] == '@') {
+                    neighbor_count++;
+                }
+                if (grid[row - 1][col] == '@') {
+                    neighbor_count++;
+                }
+                if (grid[row - 1][col + 1] == '@') {
+                    neighbor_count++;
+                }
+                if (grid[row][col - 1] == '@') {
+                    neighbor_count++;
+                }
+                if (grid[row][col + 1] == '@') {
+                    neighbor_count++;
+                }
+                if (grid[row + 1][col - 1] == '@') {
+                    neighbor_count++;
+                }
+                if (grid[row + 1][col] == '@') {
+                    neighbor_count++;
+                }
+                if (grid[row + 1][col + 1] == '@') {
+                    neighbor_count++;
+                }
+                if (neighbor_count < max_adjacent) {
+                    count++;
+                }
+            } else {
+                continue;
             }
         }
     }
-    return 0;
+    return count;
 }
