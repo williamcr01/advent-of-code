72d37635ad35c24fd95986c0ef3076351580b30c
williamcr01 <williamcrona@gmail.com>
2026-06-08 12:16:17 +0200

    day-5-1 rewrite

--- 2025/day-5/day-5-1.c
+++ 2025/day-5/day-5-1.c
@@ -1,46 +1,40 @@
 #include <stdio.h>
-#include <stdbool.h>
+
+typedef struct {
+    long long start;
+    long long end;
+} Range;
 
 int main() {
-    int fresh = 0;
-    
+    Range ranges[1000];
     long long ids[1000];
-    long long starts[1000];
-    long long ends[1000];
-
-    bool scanning_ranges = true;
     int range_count = 0;
     int id_count = 0;
 
     char line[1024];
     while (fgets(line, sizeof(line), stdin)) {
         if (line[0] == '\n') {
-            scanning_ranges = false;
-            continue;
+            break;
         }
-        if (scanning_ranges) {
-            long long start, end;
-            sscanf(line, "%lld-%lld", &start, &end);
-
-            starts[range_count] = start;
-            ends[range_count] = end;
-            range_count++;
-        } else {
-            long long id;
-            sscanf(line, "%lld", &id);
-            ids[id_count] = id;
-            id_count++;
-        }
+        sscanf(line, "%lld-%lld", &ranges[range_count].start, &ranges[range_count].end);
+        range_count++;
     }
 
+    while (fgets(line, sizeof(line), stdin)) {
+        sscanf(line, "%lld", &ids[id_count]);
+        id_count++;
+    }
+
+    int fresh = 0;
     for (int i = 0; i < id_count; i++) {
         for (int j = 0; j < range_count; j++) {
-            if (ids[i] >= starts[j] && ids[i] <= ends[j]) {
+            if (ids[i] >= ranges[j].start && ids[i] <= ranges[j].end) {
                 fresh++;
                 break;
             }
         }
     }
-    printf("Fresh: %d", fresh);
+
+    printf("%d\n", fresh);
     return 0;
 }
